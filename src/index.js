d25911deedd58b5ae6622b03ae661cd3a6975ac7
Muhammad Anas <daggaanas0888@gmail.com>
2026-08-22 21:45:20 +0530

    import menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("hello checking js");
+
+import {homeContent} from "./home.js";
+
+ const home = homeContent();
+
+const homeBtn = document.querySelector(".home");
+const content = document.querySelector(".content");
+
+homeBtn.addEventListener('click',() => {
+content.append(home);
+
+});
